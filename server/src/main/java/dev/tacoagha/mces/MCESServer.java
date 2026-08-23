af02ad2db585b4809166ca892c5a74bce0996720
tacoagha <zahra4main@gmail.com>
2026-08-23 20:59:51 +0330

    Add MCES server main class

---
+++ server/src/main/java/dev/tacoagha/mces/MCESServer.java
@@ -0,0 +1,12 @@
+package dev.tacoagha.mces;
+
+/**
+ * MCES Server - Minecraft Easy Servers daemon
+ */
+public class MCESServer {
+    
+    public static void main(String[] args) {
+        System.out.println("MCES Server starting...");
+        // TODO: Implement server initialization
+    }
+}
